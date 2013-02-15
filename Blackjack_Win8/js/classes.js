e49630e35952066b674ee7825fcc9586eb5723af
David Moen <davidamoen@gmail.com>
2013-02-15 13:13:07 -0600

    Add comments and new shoe and hand classes

--- Blackjack_Win8/js/classes.js
+++ Blackjack_Win8/js/classes.js
@@ -55,13 +55,13 @@
             },
             HighValue: function () {
 
-                var vals = this.CardValues;
+                var vals = this.CardValues();
                 vals.sort();
                 return vals[vals.length - 1];
 
             },
             LowValue: function () {
-                var vals = this.CardValues;
+                var vals = this.CardValues();
                 vals.sort();
                 return vals[0];
             }
@@ -119,29 +119,51 @@ function (deckCount) {
             }
         }
     },
+    // logic for shuffling cards
+    // tried to emulate a human shuffling cards rather than simply randomizing our array
     Shuffle: function (shuffleCount) {
 
+        // repeat the shuffling logic shuffleCount times
+        // research says that 7 shuffles is needed to really randomize a deck of cards 
         for (var i = 0; i < shuffleCount; i++) {
+
+            // create variables that will help determine where we cut the cards
             var newShoe = [];
             var midPoint = this.Cards.length / 2;
             var spread = 10;
 
+            // use a random cut of the cards within a certain range
             var cut = SDG.rand(midPoint - spread, midPoint + spread);
+
+            // create first pile of cards
             var cutA = this.Cards.slice(0, cut);
+
+            // reverse them so we can deal them properly
             cutA.reverse();
 
+            // create second pile of cards
             var cutB = this.Cards.slice(cut, this.Cards.length);
+
+            // reverse them so we can deal them properly
             cutB.reverse();
 
+            // loop until one of the piles is empty
+            // a riffle is a small number of cards from one pile that are interlaid 
+            // with a riffle from the second pile
             while (cutA.length > 0 && cutB.length > 0) {
 
+                // determinethe size of the first riffle and move that 
+                // number of cards from the pile to the newShoe pile
                 var riffleA = SDG.rand(1, 5);
                 newShoe = newShoe.concat(cutA.splice(0, riffleA));
 
+                // repeat for the second pile
                 var riffleB = SDG.rand(1, 5);
                 newShoe = newShoe.concat(cutB.splice(0, riffleB));
             }
 
+            // after one of the piles is empty, some cards remain in the other
+            // add those to the end of the shoe
             if (cutA.length > 0) {
                 newShoe = newShoe.concat(cutA);
             }
@@ -151,9 +173,25 @@ function (deckCount) {
             }
         }
 
+        this.Cards = newShoe;
 
+    },
+    // vegas dealers put a red card in the shoe
+    // when they reach that red card, they reshuffle
+    // this is effective discarding all cards in the shoe after the red card
+    AddRedCard: function () {
+        // determine the low end of the range where the red card can go
+        var low = this.Cards.length - Math.floor(this.Cards.length * .20);
+
+        // determine the high end of the range
+        var high = this.Cards.length;
+
+        // select a random number from within the range
+        var cardPosition = SDG.rand(low, high);
+
+        // discard all cards after the position selected
+        this.Cards = this.Cards.slice(0, cardPosition);
     }
-
 });
 
 
@@ -161,6 +199,98 @@ function (deckCount) {
 // end Shoe class
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Hand class
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+var Hand = WinJS.Class.define(
+    function () { },
+    {
+        Cards: [],
+        Values: function () {
+            var vals = [];
+            for (var cardIdx in this.Cards) {
+                var card = this.Cards[cardIdx];
+                if (card.HighValue() == card.LowValue()) {
+                    if (vals.length == 0) {
+                        vals.push(card.HighValue());
+                    }
+                    else {
+                        var len = vals.length;
+                        for (var valIdx = 0; valIdx < len; valIdx++) {
+                            vals[valIdx] += card.HighValue();
+                        }
+                    }
+                }
+                else {
+
+                    if (vals.length == 0) {
+                        vals.push(card.HighValue());
+                        vals.push(card.LowValue());
+                    }
+                    else {
+                        var newList = [];
+                        var len = vals.length;
+                        for (var valIdx = 0; valIdx < len; valIdx++) {
+                            newList.push(vals[valIdx] + card.LowValue());
+                            vals[valIdx] += card.HighValue();
+                        }
+                        vals.concat(newList);
+                    }
+                }
+            }
+            return vals;
+        },
+        HighValue: function () {
+            var values = this.Values();
+            values = values.sort(function (num1, num2) {
+                return num1 - num2;
+            });
+            return values[values.length - 1];
+        },
+        LowValue: function () {
+            var values = this.Values();
+            values = values.sort(function (num1, num2) {
+                return num1 - num2;
+            });
+            return values[0];
+        },
+        IsBust: function () {
+            var isBust = true;
+            for (var valIdx in this.Values()) {
+                if (this.Values()[valIdx] <= 21) {
+                    isBust = false;
+                    break;
+                }
+            }
+        },
+        IsBlackJack: function () {
+            return this.Cards.length == 2
+                    && (this.Cards[0].HighValue() == 11 || this.Cards[1].HighValue() == 11)
+                    && (this.Cards[0].HighValue() == 10 || this.Cards[1].HighValue() == 10);
+
+        },
+        IsSplittable: function () {
+            return this.Cards.length == 2 && this.Cards[0].CardType == this.Cards[1].CardType;
+        },
+        HasAce: function () {
+            var hasAce = false;
+            for (var cardIdx in this.Cards) {
+                if (this.Cards[cardIdx].CardType == "Ace") {
+                    hasAce = true;
+                    break;
+                }
+            }
+            return hasAce;
+        }
+    });
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// end Hand class
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
 var Suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 
 var CardTypes = ["Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+
